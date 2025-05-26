72a1ff9b85e4e4101b55c8c623830410dd7861aa
Andor Mihaly <andor_mihaly@yahoo.com>
2025-05-26 14:28:23 +0300

    Add Domain project

---
+++ Taxually.TechnicalTest/Taxually.Domain/VatRegistrationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Taxually.Domain
+{
+    public class VatRegistrationRequest
+    {
+        public string CompanyName { get; set; }
+        public string CompanyId { get; set; }
+        public string Country { get; set; }
+    }
+}
